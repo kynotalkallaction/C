57eee6ebe5c70502dd0e5c79c83110b786109adc
Phạm Trung Kỳ <44391211+phamtrungky18it4@users.noreply.github.com>
2018-10-23 15:43:59 +0700

    Add files via upload

---
+++ BaiTapMang_01.cpp
@@ -0,0 +1,80 @@
+/*Bai tap Mang*/
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void  NhapMang(int arr[100],int n){
+	int i;
+	for ( i = 0; i < n; ++i){
+		printf("a[%d] = ",i);
+		scanf("%d",& arr[i]);		
+	}
+}
+void InMang(int arr[100],int n){
+	printf("Mang ban vua nhap :\n");
+	for (int i = 0; i < n; ++i)
+	printf(" a[%d]= %d\n",i,arr[i]);
+}
+
+long giaithua (int n)
+{	int i;
+    long giaithua = 1;
+    printf("\n Nhap n: ");
+    scanf("%d", &n);
+    for( i = 1; i <= n; i++)
+    {
+        giaithua=giaithua*i;
+    }
+    printf("\n Giai thua la:  %ld\n", giaithua);
+       
+}
+int Chuoi(char chuoi[100]){
+	int i=0,dem,kt=0;
+	printf("Nhap vao chuoi bat ky :");
+	scanf("%s",chuoi);
+	printf("Chuoi da nhap la : %s \n",chuoi);
+
+	dem= strlen(chuoi);
+
+	
+}
+double TienDien(int csm,int csc){
+	float tien,socu,somoi;
+	printf("Nhap so moi : \n");
+	scanf("%f",& somoi);
+	printf("Nhap so cu :\n");
+	scanf("%f",& socu);
+	if (somoi > socu){
+		int tieuthu=somoi-socu;
+		if ( tieuthu< 50)
+		{ 
+		tien=tieuthu*1500;
+		printf("tien dien la : %f",tien);
+		}
+		else if (tieuthu>=50 && tieuthu<100)
+		{ 
+		tien=50*1500+(tieuthu-50)*2000;
+		printf("tien dien la : %f",tien);
+		}
+		else {
+			tien=50*1500+100*2000+(tieuthu-150)*3000;
+			printf("tien dien la : %f",tien);
+		}
+		}
+	else 
+	printf(" Co the ban da sai ! ( So moi < So cu )");
+	
+}
+int main(){
+	int mang[100];
+	char c [100];
+	int csm,csc;
+	int n;//so phan tu muon nhap vao mang
+	printf("Nhap n= ");
+	scanf("%d",&n);
+	NhapMang(mang,n);
+	InMang(mang,n);
+	Chuoi(c);
+	giaithua(n);
+	TienDien(csm,csc);
+	return 0;
+}
